6cabebe4612c99ff67f9f65e799ce27d7717ddcf
AlexandrASUTP <sasha.krivosheev.82@mail.ru>
2022-04-11 10:03:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Юнит_3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Sasha";
+            Console.WriteLine(myName);
+        }
+    }
+}
